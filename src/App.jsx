f3eb10e483bd22438f8ad5e8a00e4af609d1567d
Preslav977 <p.cvetanow@gmail.com>
2024-07-30 13:05:13 +0300

    Deleted the second use Effect, since it was racing with the first one and it was returning undefined also deleted the first state.

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,33 @@ import DropDownMenuContent from "./components/DropDownMenuContent";
 import "./App.css";
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [coordinates, setCoordinates] = useState({ x: 0, y: 0 });
   const [imageWidthAndHeight, setImageWidthAndHeight] = useState({
     width: 0,
     height: 0,
   });
 
+  useEffect(() => {
+    fetch("http://localhost:3000/character", {
+      mode: "cors",
+    })
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("Server Error");
+        }
+        return response.json();
+      })
+      .then((response) => setCharacters(response))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, [setCharacters]);
+
   const targetingBoxDimension = 70;
 
-  const divideTargetingBox = targetingBoxDimension / 2;
+  const centerTargetingBox = targetingBoxDimension / 2;
 
   const dropdownRef = useRef(null);
 
@@ -23,7 +41,7 @@ function App() {
     function getCoordinates(e) {
       const rect = e.target.getBoundingClientRect();
 
-      console.log("Width", rect.width, "Height", rect.height);
+      // console.log("Width", rect.width, "Height", rect.height);
 
       // console.log(rect);
 
@@ -58,126 +76,68 @@ function App() {
     return () => window.removeEventListener("click", getCoordinates);
   });
 
-  function printCoordinates() {
+  function normalizeCoordinates() {
     const copyCoords = { ...coordinates };
 
-    //Raft Man Coordinates Big Screen 98 X, 1140 Y, 1915px width, 2702.90625 px height
-
-    //Normalized Coordinates X 5,12, Coordinate Y 42.18 Big Screen
-
-    //Raft Man Coordinates Small Screen 45 X 519 Y, 876px width, 1236.40625px height
-
-    //Normalized Coordinates X 5,14 Coordinate Y 41.98 Small Screen
-
-    //////////////////////////////////////////////////////////////////////////////////
-
-    //Dragon Coordinates Big Screen 1269 X, 1149 Y, 1915px width, 2702.90625 px height
-
-    //Normalized Coordinates X 66.27, Coordinate Y 42.51 Big Screen
-
-    //Dragon Coordinates Small Screen 579 X, 523 Y, 874px width, 1233.59375 px height
-
-    //Normalized Coordinates X 66.25. Coordinate Y 42.40 Small Screen
-
-    /////////////////////////////////////////////////////////////////////////////////
-
-    //Wizard Coordinates Big Screen 1453 X, 1780 Y, 1915px width, 2702.90625 px height
-
-    //Normalized Coordinates X 75.87, 65.85 Y Big Screen
-
-    //Wizard Coordinates Small Screen 661 X, 812 Y, 874px width, 1233.59375 px height
-
-    //Normalized Coordinates X 75.73, 65.82 Y Small Screen
-
-    console.log(copyCoords);
-
-    const normalizedCoordRaftManX = 5.12;
-
-    const normalizedCoordRaftManY = 42.18;
-
-    const normalizedCoordDragonX = 66.27;
-
-    const normalizedCoordDragonY = 42.51;
-
-    const normalizedCoordWizardX = 75.87;
-
-    const normalizedCoordWizardY = 65.85;
+    // console.log(coordinates);
 
     const findLowerBoundX =
-      ((copyCoords.x - divideTargetingBox) / imageWidthAndHeight.width) * 100;
+      ((copyCoords.x - centerTargetingBox) / imageWidthAndHeight.width) * 100;
 
     const findLowerBoundY =
-      ((copyCoords.y - divideTargetingBox) / imageWidthAndHeight.height) * 100;
-
-    console.log(findLowerBoundX, findLowerBoundY);
+      ((copyCoords.y - centerTargetingBox) / imageWidthAndHeight.height) * 100;
 
     const findUpperBoundX =
-      ((copyCoords.x + divideTargetingBox) / imageWidthAndHeight.width) * 100;
+      ((copyCoords.x + centerTargetingBox) / imageWidthAndHeight.width) * 100;
 
     const findUpperBoundY =
-      ((copyCoords.y + divideTargetingBox) / imageWidthAndHeight.height) * 100;
+      ((copyCoords.y + centerTargetingBox) / imageWidthAndHeight.height) * 100;
 
-    console.log(findUpperBoundX, findUpperBoundY);
+    const mousePositionsObject = {
+      lowerX: findLowerBoundX,
+      upperX: findUpperBoundX,
+      lowerY: findLowerBoundY,
+      upperY: findUpperBoundY,
+    };
 
-    // if (
-    //   normalizedCoordRaftManX <= findLowerBoundX ||
-    //   normalizedCoordRaftManX >= findUpperBoundX ||
-    //   normalizedCoordRaftManY <= findLowerBoundY ||
-    //   normalizedCoordRaftManY >= findUpperBoundY
-    // ) {
-    //   console.log("Target not found");
-    // } else {
-    //   console.log("You found Raft Man");
-    // }
-
-    // if (
-    //   normalizedCoordDragonX <= findLowerBoundX ||
-    //   normalizedCoordDragonX >= findUpperBoundX ||
-    //   normalizedCoordDragonY <= findLowerBoundY ||
-    //   normalizedCoordDragonY >= findUpperBoundY
-    // ) {
-    //   console.log("Target not found");
-    // } else {
-    //   console.log("You found Dragon.");
-    // }
-
-    // if (
-    //   normalizedCoordWizardX <= findLowerBoundX ||
-    //   normalizedCoordWizardX >= findUpperBoundX ||
-    //   normalizedCoordWizardY <= findLowerBoundY ||
-    //   normalizedCoordWizardY >= findUpperBoundY
-    // ) {
-    //   console.log("Target not found");
-    // } else {
-    //   console.log("You found Wizard.");
-    // }
+    console.log(mousePositionsObject);
   }
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>A network error was encountered</p>;
+  }
+
+  console.log(characters);
 
   return (
     <>
       <NavComponent
         gameTime="0:00:00"
-        firstCharImg="raft-man.png"
+        firstCharImg={characters[0].character_image}
         firstCharImgDesc="Raft Man character"
-        firstCharName="Raft Man"
-        secondCharImg="wizard.png"
+        firstCharName={characters[0].character_name}
+        secondCharImg={characters[1].character_image}
         secondCharImgDesc="Wizard character"
-        secondCharName="Wizard"
-        thirdCharImg="dragon.png"
+        secondCharName={characters[1].character_name}
+        thirdCharImg={characters[2].character_image}
         thirdCharImgDesc="Dragon character"
-        thirdCharName="Dragon"
+        thirdCharName={characters[2].character_name}
       />
       <div
         style={{
           display: "none",
         }}
         ref={dropdownRef}
-        onClick={printCoordinates}
+        onClick={normalizeCoordinates}
       >
         <div className="dropDownContent">
           <div
             style={{
-              transform: `translate(${coordinates.x - divideTargetingBox}px, ${coordinates.y - divideTargetingBox}px)`,
+              transform: `translate(${coordinates.x - centerTargetingBox}px, ${coordinates.y - centerTargetingBox}px)`,
             }}
             className="dropDownTargetingBox"
           >
@@ -185,30 +145,23 @@ function App() {
           </div>
           <div
             style={{
-              transform: `translate(${coordinates.x - divideTargetingBox}px, ${coordinates.y - divideTargetingBox}px)`,
+              transform: `translate(${coordinates.x - centerTargetingBox}px, ${coordinates.y - centerTargetingBox}px)`,
             }}
             className="dropDownMenu"
           >
-            <DropDownMenuContent
-              characterImgSrc="raft-man.png"
-              characterImgDesc="Raft Mam"
-              characterName="Raft Man"
-            />
-            <DropDownMenuContent
-              characterImgSrc="wizard.png"
-              characterImgDesc="Wizard"
-              characterName="Wizard"
-            />
-            <DropDownMenuContent
-              characterImgSrc="dragon.png"
-              characterImgDesc="Dragon"
-              characterName="Dragon"
-            />
+            {characters.map((character) => (
+              <DropDownMenuContent
+                key={character._id}
+                characterImgSrc={character.character_image}
+                characterImgDesc="Dragon Charmer Island characters"
+                characterName={character.character_name}
+              />
+            ))}
           </div>
         </div>
       </div>
       <MainComponent
-        gameImgSrc="dragon-charmers-island.jpg"
+        gameImgSrc={characters[0].photo.image_link}
         gameImgDesc="Dragon Charmers Island Game"
       />
       <FooterComponent />
