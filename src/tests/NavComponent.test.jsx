25b18a6a96f8f0379ab4332990c155b2a81c7415
Preslav977 <p.cvetanow@gmail.com>
2024-09-02 09:51:58 +0300

    Created second test for NavComponent but for navigation it should render the same elements

--- src/tests/NavComponent.test.jsx
+++ src/tests/NavComponent.test.jsx
@@ -6,9 +6,46 @@ import {
 import { RouterProvider, createMemoryRouter } from "react-router-dom";
 import { describe, expect, it } from "vitest";
 import routes from "../router/routes";
+import userEvent from "@testing-library/user-event";
 
 describe("Should render NavComponent", () => {
   it("Should render NavComponent", async () => {
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/"],
+    });
+    render(<RouterProvider router={router}></RouterProvider>);
+    // screen.debug();
+
+    const apiLoading = screen.queryByAltText("Loading...");
+
+    expect(apiLoading).toBeInTheDocument();
+
+    await waitForElementToBeRemoved(() => screen.queryByAltText("Loading..."));
+
+    // screen.debug();
+
+    expect(screen.queryByText("Character").textContent).toMatch(/Character/i);
+
+    expect(screen.queryByText("Hunt").textContent).toMatch(/Hunt/i);
+
+    expect(screen.queryByText("Leaderboard").textContent).toMatch(
+      /Leaderboard/i,
+    );
+
+    expect(screen.queryByText("Games").textContent).toMatch(/Games/i);
+
+    expect(screen.queryByText("Leaderboard").textContent).toMatch(
+      /Leaderboard/i,
+    );
+
+    expect(screen.queryByText("Dragon Charmers Island").textContent).toMatch(
+      /Dragon Charmers Island/i,
+    );
+
+    expect(screen.queryByText("Start Game").textContent).toMatch(/Start Game/i);
+  });
+
+  it("Should NavComponent navigate to Leaderboard Link", async () => {
     const router = createMemoryRouter(routes, {
       initialEntries: ["/"],
     });
@@ -20,6 +57,14 @@ describe("Should render NavComponent", () => {
     expect(apiLoading).toBeInTheDocument();
 
     await waitForElementToBeRemoved(() => screen.queryByAltText("Loading..."));
+
+    screen.debug();
+
+    const user = userEvent.setup();
+
+    const LeaderboardLink = screen.queryByText("Leaderboard");
+
+    await user.click(LeaderboardLink);
 
     screen.debug();
 
