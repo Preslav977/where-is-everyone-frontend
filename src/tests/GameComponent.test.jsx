f4f522d9079f73f43f3e1a82603a8784158be5e0
Preslav977 <p.cvetanow@gmail.com>
2024-09-02 10:52:22 +0300

    Created second test in GameComponent to not render the button

--- src/tests/GameComponent.test.jsx
+++ src/tests/GameComponent.test.jsx
@@ -45,4 +45,40 @@ describe("Should render GameComponent", () => {
 
     expect(screen.queryByText("Start Game").textContent).toMatch(/Start Game/i);
   });
+
+  it("should render GameComponent with props and button shouldnt be rendered", () => {
+    const routes = [
+      {
+        path: "/",
+
+        element: (
+          <GameComponent
+            gameImage={"http://localhost:3000/dragon-charmers-island.jpg"}
+            gameImageDescription={"Dragon Charmers Island"}
+            gameName={"Dragon Charmers Island"}
+            showButton={false}
+            gameLink={"/66d1a12dcec8c4497322b73e"}
+          />
+        ),
+      },
+    ];
+
+    const router = createMemoryRouter(routes, {});
+
+    render(<RouterProvider router={router} />);
+
+    screen.debug();
+
+    expect(screen.queryByAltText("Dragon Charmers Island"));
+
+    expect(screen.queryByRole("img").src).toEqual(
+      "http://localhost:3000/dragon-charmers-island.jpg",
+    );
+
+    expect(screen.queryByText("Dragon Charmers Island").textContent).toMatch(
+      /Dragon Charmers Island/i,
+    );
+
+    expect(screen.queryByText("Start Game")).not.toBeInTheDocument();
+  });
 });
