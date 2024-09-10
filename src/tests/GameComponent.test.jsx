e6381db4bbd46c6b46c65eea8a8fc9ba807772b8
Preslav977 <p.cvetanow@gmail.com>
2024-09-10 10:41:34 +0300

    Added two more tests with passed props for the two games in GameComponent, but I had to change the image source

--- src/tests/GameComponent.test.jsx
+++ src/tests/GameComponent.test.jsx
@@ -5,7 +5,7 @@ import routes from "../router/routes";
 
 import GameComponent from "../components/GameComponent";
 
-describe("Should render GameComponent", () => {
+describe("Should render GameComponent with Dragon Charmer's Island passed props", () => {
   it("should render GameComponent with props", () => {
     const routes = [
       {
@@ -15,7 +15,7 @@ describe("Should render GameComponent", () => {
           <GameComponent
             gameImage={"http://localhost:3000/dragon-charmers-island.jpg"}
             gameImageDescription={"Dragon Charmers Island"}
-            gameName={"Dragon Charmers Island"}
+            gameName={"Dragon Charmer's Island"}
             showButton={true}
             gameLink={"/66d1a12dcec8c4497322b73e"}
           />
@@ -35,14 +35,14 @@ describe("Should render GameComponent", () => {
       "http://localhost:3000/dragon-charmers-island.jpg",
     );
 
-    expect(screen.queryByText("Dragon Charmers Island").textContent).toMatch(
-      /Dragon Charmers Island/i,
+    expect(screen.queryByText("Dragon Charmer's Island").textContent).toMatch(
+      /Dragon Charmer's Island/i,
     );
 
     expect(screen.queryByText("Start Game").textContent).toMatch(/Start Game/i);
   });
 
-  it("should render GameComponent with props and button shouldnt be rendered", () => {
+  it("should render GameComponent with props and button shouldn't be rendered", () => {
     const routes = [
       {
         path: "/",
@@ -51,7 +51,7 @@ describe("Should render GameComponent", () => {
           <GameComponent
             gameImage={"http://localhost:3000/dragon-charmers-island.jpg"}
             gameImageDescription={"Dragon Charmers Island"}
-            gameName={"Dragon Charmers Island"}
+            gameName={"Dragon Charmer's Island"}
             showButton={false}
             gameLink={"/66d1a12dcec8c4497322b73e"}
           />
@@ -65,14 +65,162 @@ describe("Should render GameComponent", () => {
 
     // screen.debug();
 
-    expect(screen.queryByAltText("Dragon Charmers Island"));
+    expect(screen.queryByAltText("Dragon Charmer's Island"));
 
     expect(screen.queryByRole("img").src).toEqual(
       "http://localhost:3000/dragon-charmers-island.jpg",
     );
 
-    expect(screen.queryByText("Dragon Charmers Island").textContent).toMatch(
-      /Dragon Charmers Island/i,
+    expect(screen.queryByText("Dragon Charmer's Island").textContent).toMatch(
+      /Dragon Charmer's Island/i,
+    );
+
+    expect(screen.queryByText("Start Game")).not.toBeInTheDocument();
+  });
+});
+
+describe("Should render GameComponent Super Mario Bros with passed props", () => {
+  it("should render GameComponent with props", () => {
+    const routes = [
+      {
+        path: "/",
+
+        element: (
+          <GameComponent
+            gameImage={"http://localhost:3000/super-mario-bros.jpg"}
+            gameImageDescription={"Dragon Charmers Island"}
+            gameName={"Super Mario Bros"}
+            showButton={true}
+            gameLink={"/66d1a12dcec8c4497322b73e"}
+          />
+        ),
+      },
+    ];
+
+    const router = createMemoryRouter(routes, {});
+
+    render(<RouterProvider router={router} />);
+
+    // screen.debug();
+
+    expect(screen.queryByAltText("Super Mario Bros"));
+
+    expect(screen.queryByRole("img").src).toEqual(
+      "http://localhost:3000/super-mario-bros.jpg",
+    );
+
+    expect(screen.queryByText("Super Mario Bros").textContent).toMatch(
+      /Super Mario Bros/i,
+    );
+
+    expect(screen.queryByText("Start Game").textContent).toMatch(/Start Game/i);
+  });
+
+  it("should render GameComponent with props and button shouldn't be rendered", () => {
+    const routes = [
+      {
+        path: "/",
+
+        element: (
+          <GameComponent
+            gameImage={"http://localhost:3000/super-mario-bros.jpg"}
+            gameImageDescription={"Dragon Charmers Island"}
+            gameName={"Super Mario Bros"}
+            showButton={false}
+            gameLink={"/66dec67b9ab29781c1656ffa"}
+          />
+        ),
+      },
+    ];
+
+    const router = createMemoryRouter(routes, {});
+
+    render(<RouterProvider router={router} />);
+
+    // screen.debug();
+
+    expect(screen.queryByAltText("Super Mario Bros"));
+
+    expect(screen.queryByRole("img").src).toEqual(
+      "http://localhost:3000/super-mario-bros.jpg",
+    );
+
+    expect(screen.queryByText("Super Mario Bros").textContent).toMatch(
+      /Super Mario Bros/i,
+    );
+
+    expect(screen.queryByText("Start Game")).not.toBeInTheDocument();
+  });
+});
+
+describe("Should render GameComponent Universe 133 with passed props ", () => {
+  it("should render GameComponent with props", () => {
+    const routes = [
+      {
+        path: "/",
+
+        element: (
+          <GameComponent
+            gameImage={"http://localhost:3000/universe-133.jpg"}
+            gameImageDescription={"Dragon Charmers Island"}
+            gameName={"Universe 133"}
+            showButton={true}
+            gameLink={"/66d1a12dcec8c4497322b73e"}
+          />
+        ),
+      },
+    ];
+
+    const router = createMemoryRouter(routes, {});
+
+    render(<RouterProvider router={router} />);
+
+    // screen.debug();
+
+    expect(screen.queryByAltText("Universe 133"));
+
+    expect(screen.queryByRole("img").src).toEqual(
+      "http://localhost:3000/universe-133.jpg",
+    );
+
+    expect(screen.queryByText("Universe 133").textContent).toMatch(
+      /Universe 133/i,
+    );
+
+    expect(screen.queryByText("Start Game").textContent).toMatch(/Start Game/i);
+  });
+
+  it("should render GameComponent with props and button shouldn't be rendered", () => {
+    const routes = [
+      {
+        path: "/",
+
+        element: (
+          <GameComponent
+            gameImage={"http://localhost:3000/universe-133.jpg"}
+            gameImageDescription={"Dragon Charmers Island"}
+            gameName={"Universe 133"}
+            showButton={false}
+            gameLink={"/66d1a12dcec8c4497322b73e"}
+          />
+        ),
+      },
+    ];
+
+    const router = createMemoryRouter(routes, {});
+
+    render(<RouterProvider router={router} />);
+
+    // screen.debug();
+
+    expect(screen.queryByAltText("Universe 133"));
+
+    expect(screen.queryByRole("img").src).toEqual(
+      "http://localhost:3000/universe-133.jpg",
+    );
+
+    expect(screen.queryByText("Universe 133").textContent).toMatch(
+      /Universe 133/i,
     );
 
     expect(screen.queryByText("Start Game")).not.toBeInTheDocument();
