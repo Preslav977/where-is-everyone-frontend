c66f4a532078984429e42b9db138334c3948bd2b
Preslav977 <p.cvetanow@gmail.com>
2024-09-10 10:46:38 +0300

    Created similar tests for MainComponent passing different props for the two games

--- src/tests/MainComponent.test.jsx
+++ src/tests/MainComponent.test.jsx
@@ -5,7 +5,7 @@ import { describe, expect, it } from "vitest";
 import MainComponent from "../components/MainComponent";
 
 describe("Should render MainComponent", () => {
-  it("Should render MainComponent with props", () => {
+  it("Should render MainComponent Dragon Charmer's Island with props", () => {
     const routes = [
       {
         path: "/",
@@ -15,7 +15,7 @@ describe("Should render MainComponent", () => {
             className={"mainContainer"}
             position={"fixed"}
             gameImageSrc={"http://localhost:3000/dragon-charmers-island.jpg"}
-            gameImageDescription={"Dragon Charmers Island"}
+            gameImageDescription={"Dragon Charmer's Island"}
           />
         ),
       },
@@ -35,6 +35,72 @@ describe("Should render MainComponent", () => {
       "http://localhost:3000/dragon-charmers-island.jpg",
     );
 
-    expect(screen.queryByRole("img").alt).toEqual("Dragon Charmers Island");
+    expect(screen.queryByRole("img").alt).toEqual("Dragon Charmer's Island");
+  });
+
+  it("Should render MainComponent Super Mario Bros with props", () => {
+    const routes = [
+      {
+        path: "/",
+
+        element: (
+          <MainComponent
+            className={"mainContainer"}
+            position={"fixed"}
+            gameImageSrc={"http://localhost:3000/super-mario-bros.jpg"}
+            gameImageDescription={"Super Mario Bros"}
+          />
+        ),
+      },
+    ];
+
+    const router = createMemoryRouter(routes, {});
+
+    render(<RouterProvider router={router} />);
+
+    // screen.debug();
+
+    const mainElement = screen.queryByRole("main");
+
+    expect(mainElement, { name: "mainContainer" });
+
+    expect(screen.queryByRole("img").src).toEqual(
+      "http://localhost:3000/super-mario-bros.jpg",
+    );
+
+    expect(screen.queryByRole("img").alt).toEqual("Super Mario Bros");
+  });
+
+  it("Should render MainComponent Universe 133 with props", () => {
+    const routes = [
+      {
+        path: "/",
+
+        element: (
+          <MainComponent
+            className={"mainContainer"}
+            position={"fixed"}
+            gameImageSrc={"http://localhost:3000/universe-133.jpg"}
+            gameImageDescription={"Universe 133"}
+          />
+        ),
+      },
+    ];
+
+    const router = createMemoryRouter(routes, {});
+
+    render(<RouterProvider router={router} />);
+
+    // screen.debug();
+
+    const mainElement = screen.queryByRole("main");
+
+    expect(mainElement, { name: "mainContainer" });
+
+    expect(screen.queryByRole("img").src).toEqual(
+      "http://localhost:3000/universe-133.jpg",
+    );
+
+    expect(screen.queryByRole("img").alt).toEqual("Universe 133");
   });
 });
